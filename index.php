ae5985b443744f8525d544d8b9dfd34e8cabe1f7
dimonpvt <dimonpvt@mail.ru>
2013-10-02 14:42:12 +0400

    start work

--- index.php
+++ index.php
@@ -1,100 +1,120 @@
 <?php
-    session_start();
-    header('Content-Type: text/html; charset=utf-8');
-?>
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="utf-8">
-    <title>Главная | Casting@Online</title>
-    <meta name="description" content="casting">
-    <meta name="author" content="Reaper Germany">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
-    <link href='css/boxer.css' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/elusive-webfont.css">
-    <script src="js/jquery.min.js" type="text/javascript"></script>
-    <script src="js/jquery.smooth-scroll.js"></script>
-    <script src="js/boxer.js"></script>
-	<link href='http://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-	<!-- elusive icons ie7 support -->
-    <!--[if lte IE 7]><script src="js/lte-ie7.js"></script><![endif]-->
+date_default_timezone_set('America/New_York');
+/*
+|---------------------------------------------------------------
+| PHP ERROR REPORTING LEVEL
+|---------------------------------------------------------------
+|
+| By default CI runs with error reporting set to ALL.  For security
+| reasons you are encouraged to change this when your site goes live.
+| For more info visit:  http://www.php.net/error_reporting
+|
+*/
+	error_reporting(E_ALL);
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-    <![endif]-->
-    <style>
-      body {
-        padding-top: 50px; /* Only include this for the fixed top bar */
-      }
-    </style>
-  </head>
-  <body id="top">
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="#top">Casting@Online</a>
+/*
+|---------------------------------------------------------------
+| SYSTEM FOLDER NAME
+|---------------------------------------------------------------
+|
+| This variable must contain the name of your "system" folder.
+| Include the path if the folder is not in the same  directory
+| as this file.
+|
+| NO TRAILING SLASH!
+|
+*/
+	//$system_folder = "system";
+        $system_folder = "";
 
-          <ul class="nav boxer_nav">
-            <li><a href="*">Кастинги</a></li>
-            <li><a href="*">FAQ</a></li>
-            <li><a href="*">О Нас </a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div id="et-faces"></div>
-<div class="container">
-	
-	<div id="landing-tab">
-		<div>
-		<h1>Casting@Online.ru </br>Все кастинги России</h1>
-		<a class="btn btn-large btn-success"; href="public_index.php">Войти</a>
-		</div>
-	</div>
-	</div>
-		<div class="container">
-			<div class="span12 et-split">
-				<p><strong>Casting@Online.ru</strong> - новый сервис кастингов. Наш сервис представляет собой бесплатный, удобный и простой инструмент где каждый может провести кастинги и отобрать артистов в свои проекты.</p>
-				<p><strong>Этот сервис</strong> - продукт работы команды профессионалов, мы надеемся, что наши многолетние труды упростят повседневные задачи кастинг-директоров, агентов, режиссёров и откроют им новые возможности, которых не было ранее или которые раньше были недоступны или очень дороги для развивающейся индустрии.</p>
-				<p><strong>Цель нашей работы</strong> - развитие отраслей отечественного кино- модельного- и шоу-бизнеса. Мы приложили массу усилий для организации работы этого сервиса и будем прилагать ещё больше усилий в будущем для его развития, а вместе с ним и развития индустрии.</p>
-				<div class="row">
-				<div class="span6 et-split">
-				<h4><strong>Для тех кто ищет таланты</strong></h4>
-				<p>Мы не являемся актёрским агентством или продюсерским центром, наш сервис абсолютно прозрачен, бесплатен и открыт для всех профессионалов индустрии или начинающих артистов! Мы не вмешиваемся в вашу работу и не являемся посредниками в любых вопросах.</p>
-				<p>С помощью нашего сайта вы легко можете найти подходящий типаж в обширной базе анкет, либо можете объявить кастинг и нужные люди найдут вас сами! Нами разработан функционал, позволяющий удобно обрабатывать заявки от пользователей и создавать собственную базу анкет под каждый отдельный проект.</p>
-				</div>
-				<div class="span6 et-split">
-				<h4><strong>Для тех кто ищет работу</strong></h4>
-				<p>Мы модерируем все поступающие кастинги чтобы предотвратить любые мошеннические схемы, благодаря чему наш сайт не похож на беспорядочную доску объявлений, которых сотни в интернете. После модерации кастинги попадают в международную почтовую рассылку по подписчикам, таким образом, десятки тысяч артистов очень быстро узнают о проходящих кастингах!</p>
-				<p>Наш сайт посещают около 20 тысяч человек каждый день. Среди них не только артисты, но и огромное количество кастинг-директоров, агентов, режиссёров, продюсеров и множество других нанимателей из всех сфер кино- модельного- и шоу-бизнеса. Если у вас есть талант - вас обязательно найдут!</p>
-				</div>
-				</div>
-	           	<p style="text-align:center"><strong>Огромный каталог талантов, простота в управлении и профессионализм нашего менеджмента сделают наше с вами сотрудничество максимально удобным и продуктивным!</strong></p>
-			</div>
-		</div>
-     <div class="boxer_footer">
-      <div class="container">
-        <div class="row">
-          <div class="span12">
-            <ul>
-              <li><a href="">Кастинги</a></li>
-              <li><a href="">FAQ</a></li>
-              <li><a href="">О Нас</a></li>
-            </ul>
-            <div class="boxer_copyright" style="float:right; margin-top: 20px">
-              &copy; 2013 Casting@Online
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+/*
+|---------------------------------------------------------------
+| APPLICATION FOLDER NAME
+|---------------------------------------------------------------
+|
+| If you want this front controller to use a different "application"
+| folder then the default one you can set its name here. The folder 
+| can also be renamed or relocated anywhere on your server.
+| For more info please see the user guide:
+| http://codeigniter.com/user_guide/general/managing_apps.html
+|
+|
+| NO TRAILING SLASH!
+|
+*/
+	$application_folder = "application";
+
+/*
+|===============================================================
+| END OF USER CONFIGURABLE SETTINGS
+|===============================================================
+*/
+
+
+/*
+|---------------------------------------------------------------
+| SET THE SERVER PATH
+|---------------------------------------------------------------
+|
+| Let's attempt to determine the full-server path to the "system"
+| folder in order to reduce the possibility of path problems.
+| Note: We only attempt this if the user hasn't specified a 
+| full server path.
+|
+*/
+if (strpos($system_folder, '/') === FALSE)
+{
+	if (function_exists('realpath') AND @realpath(dirname(__FILE__)) !== FALSE)
+	{
+		$system_folder = realpath(dirname(__FILE__)).'/'.$system_folder;
+	}
+}
+else
+{
+	// Swap directory separators to Unix style for consistency
+	$system_folder = str_replace("\\", "/", $system_folder); 
+}
+
+/*
+|---------------------------------------------------------------
+| DEFINE APPLICATION CONSTANTS
+|---------------------------------------------------------------
+|
+| EXT		- The file extension.  Typically ".php"
+| SELF		- The name of THIS file (typically "index.php")
+| FCPATH	- The full server path to THIS file
+| BASEPATH	- The full server path to the "system" folder
+| APPPATH	- The full server path to the "application" folder
+|
+*/
+define('EXT', '.php');
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+define('FCPATH', str_replace(SELF, '', __FILE__));
+define('BASEPATH', $system_folder.'/');
+
+if (is_dir($application_folder))
+{
+	define('APPPATH', $application_folder.'/');
+}
+else
+{
+	if ($application_folder == '')
+	{
+		$application_folder = 'application';
+	}
+
+	define('APPPATH', BASEPATH.$application_folder.'/');
+}
+
+/*
+|---------------------------------------------------------------
+| LOAD THE FRONT CONTROLLER
+|---------------------------------------------------------------
+|
+| And away we go...
+|
+*/
+require_once BASEPATH.'codeigniter/CodeIgniter'.EXT;
+
+/* End of file index.php */
+/* Location: ./index.php */
--- public_html/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once("../public_index.php");
-?>
